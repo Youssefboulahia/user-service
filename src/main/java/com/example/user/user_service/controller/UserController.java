cc7bd35c31271e20f718b64036454932cafeebba
youssefboulahia1 <boulahia.youssef@esprit.tn>
2025-05-26 01:54:22 +0200

    first commit

---
+++ src/main/java/com/example/user/user_service/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.user.user_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getById(@PathVariable Long id) {
+        return ResponseEntity.ok("User-service: getById called with id " + id);
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok("User-service: getAll called");
+    }
+}
