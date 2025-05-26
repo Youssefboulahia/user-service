51e8f0f49cb76c143ba573f49576e1191388a397
youssefboulahia1 <boulahia.youssef@esprit.tn>
2025-05-26 05:20:11 +0200

    cron job

---
+++ src/main/java/com/example/user/user_service/service/selfPingScheduler.java
@@ -0,0 +1,30 @@
+package com.example.user.user_service.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class selfPingScheduler {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final Logger logger = LoggerFactory.getLogger(selfPingScheduler.class);
+
+    // Runs every 5 minutes (300,000 milliseconds)
+    @Scheduled(fixedRate = 10000)
+    public void pingSelf() {
+        String url = "https://user-service-f8c4.onrender.com/users/bouza";  // or your service URL
+
+        try {
+            restTemplate.getForObject(url, String.class);
+            System.out.println("Pinged self successfully");
+            logger.info("Scheduled pingSelf() is running...");
+        } catch (Exception e) {
+            System.err.println("Failed to ping self: " + e.getMessage());
+            logger.info("Scheduled pingSelf() failed");
+        }
+    }
+}
+
